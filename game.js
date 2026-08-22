a9d2945f59de28b288b0fb7eb7e5f6b6eaf3d5c3
AKorotyaev <a.korotyaev@gmail.com>
2026-08-22 23:50:19 +0400

    small adjustments

--- game.js
+++ game.js
@@ -169,7 +169,7 @@ let audioUnlockInstalled = false;
 let soundEnabled = true;
 let backgroundMusicRequested = false;
 const activeShotSounds = new Set();
-const GAME_AUDIO_VOLUME = 0.7;
+const GAME_AUDIO_VOLUME = 0.5;
 
 const AUDIO_PATHS = {
   background: 'audio/background.mp3',
